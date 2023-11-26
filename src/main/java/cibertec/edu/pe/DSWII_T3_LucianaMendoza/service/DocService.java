2f102e85dbd627119618bb8c47c1cd3de2d63352
LucianaMendozaT <lucianamend0124@gmail.com>
2023-11-25 22:44:01 -0500

    ultimo commit

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_LucianaMendoza/service/DocService.java
@@ -0,0 +1,37 @@
+package cibertec.edu.pe.DSWII_T3_LucianaMendoza.service;
+
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class DocService {
+    private static final String UPLOAD_DIR = "Documentos";
+    private static final long MAX_FILE_SIZE = 2 * 1024 * 1024; // 2 MB
+
+    public void saveDocument(MultipartFile file) throws IOException {
+        // Verificar el tamaño del archivo
+        if (file.getSize() > MAX_FILE_SIZE) {
+            throw new FileSizeLimitExceededException();
+        }
+
+        // Verificar la extensión del archivo
+        if (!file.getOriginalFilename().toLowerCase().endsWith(".doc")) {
+            throw new FileExtensionException();
+        }
+
+        // Directorio para almacenar archivos DOC
+        File uploadDir = new File(UPLOAD_DIR);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
+        }
+
+        // Guardar el archivo en el directorio
+        String filePath = UPLOAD_DIR + "/" + file.getOriginalFilename();
+        File dest = new File(filePath);
+        file.transferTo(dest);
+    }
+}
